1ce3690929a7024261bb8f6bd9e654b6f6333266
chenyaorong666 <chenyaorong666@sina.cn>
2017-09-02 10:27:19 +0800

    first commit

---
+++ my-crawler-simple/src/main/java/com/cyr/crawler/my_crawler_simple/App.java
@@ -0,0 +1,13 @@
+package com.cyr.crawler.my_crawler_simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
