99b6585eec7a5460d88f86435e69d65d81292ee0
chenyaorong666 <chenyaorong666@sina.cn>
2017-09-02 12:19:34 +0800

    test crawler

---
+++ crawler.test/src/main/java/com/cyr/crawler/test/App.java
@@ -0,0 +1,13 @@
+package com.cyr.crawler.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
